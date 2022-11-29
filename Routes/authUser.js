b76192b6bd202291663b614e9563a216c91c834a
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-11-30 02:19:52 +0530

    authorization

--- Routes/authUser.js
+++ Routes/authUser.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const User = require("../model/auth");
-const Jwt= require("jsonwebtoken");
+const Jwt = require("jsonwebtoken");
 const authRouter = Router();
 
 // Sign up
@@ -18,29 +18,50 @@ authRouter.post("/signup", (req, res) => {
 });
 
 // Login
-authRouter.post("/login", async(req,res)=>{
-    const {username, password}= req.body;
-    const validuser= await User.find({username, password});
-    if(validuser.length < 1 || !validuser){
-        return res.status(401).send({message: "Invalid Credentials"})
+authRouter.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const validuser = await User.find({ username, password });
+  if (validuser.length < 1 || !validuser) {
+    return res.status(401).send({ message: "Invalid Credentials" });
+  }
+  // token 1
+  const token = Jwt.sign(
+    {
+      username,
+    },
+    "SECRET",
+    {
+      expiresIn: "1 hour",
     }
-    // token 1
-    const token= Jwt.sign({
-        username
+  );
+  // token 2
+  const refreshToken = Jwt.sign(
+    {
+      username,
     },
-        "SECRET",{
-            expiresIn:"1 hour"
-        }
-    ) 
-    // token 2
-    const refreshToken= Jwt.sign({
-        username
-    },
-    "REFRESHPASSWORD",{
-        expiresIn: "30days"
-    })
-    let {_id}= validuser[0]
-    return res.send({message: "Login Successfully", token: token, refreshToken: refreshToken, _id})
-})
+    "REFRESHPASSWORD",
+    {
+      expiresIn: "30days",
+    }
+  );
+  let { _id } = validuser[0];
+  return res.send({
+    message: "Login Successfully",
+    token: token,
+    refreshToken: refreshToken,
+    _id,
+  });
+});
+
+authRouter.post("/newToken", (req, res) => {
+  const refreshToken = req.headers["authorization"].split(" ")[1];
+  const validation = Jwt.verify(refreshToken, "REFRESHPASSWORD");
+  if (validation) {
+    const newPrimaryToken = Jwt.sign({ username }, "SECRET", {
+      expiresIn: "1 hour",
+    });
+    return res.send({ token: newPrimaryToken });
+  }
+});
 
 module.exports = authRouter;
